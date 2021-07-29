998544133a390c3d817d3640d2c2f71fa4538888
Diogenes de Carvalho Pereira <diogenes.pereira@luizalabs.com>
2021-07-28 21:08:43 -0300

    test: testRepositoryLibrary

---
+++ src/test/java/br/com/digitalhouse/livraria/repository/LibraryRepositoryTests.java
@@ -0,0 +1,26 @@
+package br.com.digitalhouse.livraria.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class LibraryRepositoryTests {
+
+    @Autowired
+    private LibraryRepository libraryRepository;
+
+    @Test
+    public void insertBook(){
+        Library library = new Library("TDD na Pratica", 87.20, "Digitalsers");
+        libraryRepository.save(library);
+
+        Integer countBooks = libraryRepository.findAll.size();
+        assertEquals(1, countBooks);
+    }
+}
